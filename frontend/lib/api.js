ae4558b3b4ca2d615803c29703cedc481ab0dbc8
shrutiphad <shrutiphadwork@gmail.com>
2026-05-27 23:38:41 +0530

    pdf parsing

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
 
-const api = axios.create({ baseURL: API_URL, headers: { 'Content-Type': 'application/json' } });
+
+
+const api = axios.create({ 
+  baseURL: API_URL, 
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,  
+});
 
 api.interceptors.request.use(
   (config) => {
